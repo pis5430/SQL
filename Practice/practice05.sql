2fd1b14c1c0bfd8a578b46f6d72ad2025c2e6e62
pyo i seul <isl1089@naver.com>
2020-12-23 17:32:48 +0900

    12/23 3st commit

--- Practice/practice05.sql
+++ Practice/practice05.sql
@@ -22,31 +22,60 @@ order by salary asc;
 
 /*
 문제2. 
-각 부서별로 최고의 급여를 받는 사원의 직원번호(employee_id), 이름(first_name), 급여(salary), 입사일(hire_date), 전화번호(phone_number), 부서번호(department_id) 를 조회하세요 
--조건절비교 방법으로 작성하세요
--급여의 내림차순으로 정렬하세요
+각 부서별로 최고의 급여를 받는 사원의 
+직원번호(employee_id), 이름(first_name), 급여(salary), 입사일(hire_date), 
+전화번호(phone_number), 부서번호(department_id) 를 조회하세요 
+-조건절비교 방법으로 작성하세요 (where)
+-급여의 내림차순으로 정렬하세요 (order by salary desc)
 -입사일은 2001-01-13 토요일 형식으로 출력합니다.
 -전화번호는 515-123-4567 형식으로 출력합니다.
 (11건)
 */
+--각 부서별로 최고의 급여를 받는 사원
+select department_id,
+        max(salary)
+from employees
+group by department_id,first_name;
 
+--직원번호(employee_id), 이름(first_name), 급여(salary), 입사일(hire_date), 
+--전화번호(phone_number), 부서번호(department_id) 를 조회
+select  employee_id 직원번호,
+        first_name 이름,
+        salary 급여,
+        to_char(hire_date, 'YYYY-MM-DD DAY') 입사일,
+        replace(phone_number,'.','-') 전화번호,
+        department_id 부서번호
+from employees
+where (department_id,salary) in (select department_id,
+                                        max(salary)
+                                 from employees
+                                 group by department_id)
+order by salary desc;                                 
+
+--조건절비교 방법으로 작성하세요 (where)
+--급여의 내림차순으로 정렬하세요 (order by salary desc)
+--입사일은 2001-01-13 토요일 형식으로 출력합니다. to_char(hire_date, 'YYYY-MM-DD DAY')
+--전화번호는 515-123-4567 형식으로 출력합니다. replace(phone_number,'.','-')
 
 
 /*
 문제3
 매니저별로 평균급여 최소급여 최대급여를 알아보려고 한다.
--통계대상(직원)은 2005년 이후의 입사자 입니다.
+-통계대상(직원)은 2005년 이후의 입사자 입니다. (2005년 1월 1일 이후)
 -매니저별 평균급여가 5000이상만 출력합니다.
 -매니저별 평균급여의 내림차순으로 출력합니다.
 -매니저별 평균급여는 소수점 첫째자리에서 반올림 합니다.
--출력내용은 매니저아이디, 매니저이름(first_name), 매니저별평균급여, 매니저별최소급여, 매니저별최대급여 입니다.
+-출력내용은 매니저아이디, 매니저이름(first_name), 매니저별평균급여, 
+매니저별최소급여, 매니저별최대급여 입니다.
 (9건)
 */
 
 
+
 /*
 문제4.
-각 사원(employee)에 대해서 사번(employee_id), 이름(first_name), 부서명(department_name), 매니저(manager)의 이름(first_name)을 조회하세요.
+각 사원(employee)에 대해서 사번(employee_id), 이름(first_name), 
+부서명(department_name), 매니저(manager)의 이름(first_name)을 조회하세요.
 부서가 없는 직원(Kimberely)도 표시합니다.
 (106명)
 */
@@ -68,7 +97,8 @@ order by salary asc;
 
 /*
 문제7.
-평균연봉(salary)이 가장 높은 부서 직원들의 직원번호(employee_id), 이름(firt_name), 성(last_name)과  업무(job_title), 연봉(salary)을 조회하시오.
+평균연봉(salary)이 가장 높은 부서 직원들의 직원번호(employee_id), 이름(firt_name), 
+성(last_name)과  업무(job_title), 연봉(salary)을 조회하시오.
 */
 
 
